cc0ff739a5053e898d129218d7efb7a2e688fbf9
Fernanda <fernandaign07@gmail.com>
2025-10-04 11:32:13 +0100

    Add files via upload

---
+++ addition_division.py
@@ -0,0 +1,17 @@
+# Addition and division
+print("4 + 5 =", 4 + 5)
+print("10 / 2 =", 10 / 2)
+
+# Subtraction
+print("5 - 5 =", 5 - 5)
+
+# Multiplication
+print("3 * 5 =", 3 * 5)
+# Addition and division
+
+# Subtraction
+
+# Multiplication
+ 
+ 
+ 
---
+++ create_a_variable_half.py
@@ -0,0 +1,12 @@
+# Create a variable half
+half = 0.5
+print("half =", half)
+print("type(half) =", type(half))
+# Create a variable intro
+intro = "Hello! How are you?"
+print("intro =", intro)
+print("type(intro) =", type(intro))
+# Create a variable is_good
+is_good = True
+print("is_good =", is_good)
+print("type(is_good) =", type(is_good))
